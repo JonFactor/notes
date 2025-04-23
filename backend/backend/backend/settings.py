011aa6e65880086d52d90c73fc11378a8ee726b1
JonFactor <Jon.Factor2@gmail.com>
2025-04-23 07:00:32 -0400

    rajhjj

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'flashcards',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': '21306',
         'HOST': 'localhost',
