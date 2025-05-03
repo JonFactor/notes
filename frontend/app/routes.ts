7005a088605e3cf375cbce665d1841c140397aaf
JonFactor <Jon.Factor2@gmail.com>
2025-05-03 06:26:26 -0400

    3124

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  route("/", "./routes/Home.tsx"),
+  route("/", "./routes/home.tsx"),
   route("/loading", "./routes/loading.tsx"),
   route("/generate", "./routes/generate.tsx"),
   route("/card", "./routes/card.tsx"),
